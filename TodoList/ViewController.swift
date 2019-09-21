9e91fbe7e7b04cb62959ad0bc66a44ffb6597571
duxetech <karthikriches@gmail.com>
2019-09-21 11:42:10 +0530

    add new item

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -11,12 +11,12 @@ import UIKit
 class ViewController: UITableViewController {
 
     
-    let items = ["Find dance","complete ios","Healing"]
+    var items = [String]()
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = items[indexPath.row]
-        cell.textLabel?.textAlignment = .center
+      //  cell.textLabel?.textAlignment = .center
         return cell
     }
     
@@ -36,5 +36,24 @@ class ViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add a new item", message: "", preferredStyle: .alert)
+        var tf = UITextField()
+        let action = UIAlertAction(title: "Add item", style: .default
+        ) { (action) in
+            if tf.text == "" {
+                return
+            }
+            self.items.append(tf.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField {
+            tf1 in
+            tf1.placeholder = "Enter"
+            tf = tf1
+        }
+        alert.addAction(action)
+         present(alert,animated: true,completion: nil)
+    }
 }
 
